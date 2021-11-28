cb7db1947bd8eaf4b71f91d9ded5ea1201cf9c9d
salybu <rkdmsdl93@gmail.com>
2021-11-28 13:46:06 +0900

    feat: add useQuery with params (Detail page)

--- apollo-client/src/routes/Detail.js
+++ apollo-client/src/routes/Detail.js
@@ -1,1 +1,28 @@
-export default () => "Detail";
+import { useParams } from "react-router";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      language
+      rating
+      description_intro
+    }
+  }
+`;
+
+export default () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: Number(id) },
+  });
+  if (loading) {
+    return "loading";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
+};
