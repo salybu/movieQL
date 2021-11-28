d22a4b9926adacd41303a94e127ceffaba4a9163
salybu <rkdmsdl93@gmail.com>
2021-11-28 12:09:21 +0900

    feat: add apllo client, use gql & useQuery

--- apollo-client/src/routes/Home.js
+++ apollo-client/src/routes/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
 
 const Home = () => {
-  return <div>Home</div>;
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return "loading... ";
+  }
+  if (data && data.movies) {
+    return data.movies.map((movie) => <h1>{movie.id}</h1>);
+  }
 };
 
 export default Home;
